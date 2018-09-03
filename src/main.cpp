11ba6f8685a9b04af5a99ce338feceeefc8ca63d
jin.xu <xujinchn@foxmail.com>
2018-09-03 21:23:35 +0800

    add mainWindow

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <QApplication>
+#include <iostream>
+#include <QMainWindow>
+
+int main(int argc, char *argv[])
+{
+	QApplication app(argc, argv);
+	
+	QMainWindow w;
+	w.show();
+	
+	return app.exec();
+}
